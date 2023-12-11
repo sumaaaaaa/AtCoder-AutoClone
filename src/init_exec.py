49171cc0385c5ef9b25f5eaa926ca2a55045057f
yuta saeki <sawanoi1208@gmail.com>
2023-12-12 00:18:51 +0900

    init

--- src/init_exec.py
+++ src/init_exec.py
@@ -1,5 +1,5 @@
 from autoclone import AutoClone
 
 if __name__ == "__main__":
-    time_range = (3600 * 24) * 7 * 3  # three weeks
+    time_range = (3600 * 24) * 7 * 400  # three weeks
     AutoClone(time_range)()
